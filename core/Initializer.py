e7b4b68448e2221b88eac236b074c7ed594d2f65
oh-diego-velez <diego.sanchez@outcomehealth.com>
2021-10-03 16:38:19 -0500

    comment ssm

--- core/Initializer.py
+++ core/Initializer.py
@@ -20,16 +20,16 @@ if os.path.exists(".secrets") and os.getenv('LOCAL_ENV') == "true":
     load_dotenv(".secrets")
 
 # If is not local environment, then fetch secrets from SSM and export them as env variables
-if os.getenv('LOCAL_ENV') != "true":
-    secrets_name_base = os.getenv('SECRETS_NAME_BASE')
-    aws_region = os.getenv('AWS_REGION', 'us-east-1')
-    logging.info("=== Secrets start pull")
-    secrets_manager = SecretsManager(secrets_name_base, aws_region)
-    secrets = secrets_manager.get_all_path_secrets()
-    logging.info("=== Fetched secrets")
-    # iterate through all secrets and export them as env vars     
-    for secret in secrets:
-        os.environ[secret] = secrets[secret]
+# if os.getenv('LOCAL_ENV') != "true":
+#     secrets_name_base = os.getenv('SECRETS_NAME_BASE')
+#     aws_region = os.getenv('AWS_REGION', 'us-east-1')
+#     logging.info("=== Secrets start pull")
+#     secrets_manager = SecretsManager(secrets_name_base, aws_region)
+#     secrets = secrets_manager.get_all_path_secrets()
+#     logging.info("=== Fetched secrets")
+#     # iterate through all secrets and export them as env vars     
+#     for secret in secrets:
+#         os.environ[secret] = secrets[secret]
 
 logging.info("Starting Db connection...")
 # instantiating the database at this point because secrets need to load first
